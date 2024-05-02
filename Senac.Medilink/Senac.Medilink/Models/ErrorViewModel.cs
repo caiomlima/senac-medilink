4b3e208cac9f5da23e7146089c1c2b39446ef484
Caio Moreira Lima <moreiralima.caio@gmail.com>
2024-05-02 20:29:10 -0300

    Initial commit

---
+++ Senac.Medilink/Senac.Medilink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Senac.Medilink.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
